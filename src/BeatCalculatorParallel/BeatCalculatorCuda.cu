ddb9bb583931628c757d619fff98efd3d731381b
Aashish Sinha <asinha1@andrew.cmu.edu>
2015-05-05 03:21:56 -0400

    Finished algorithm, added a few todos, need to also make sure we are freeing everything

--- src/BeatCalculatorParallel/BeatCalculatorCuda.cu
+++ src/BeatCalculatorParallel/BeatCalculatorCuda.cu
@@ -31,6 +31,132 @@ __global__ void differentiate_kernel(int size, unsigned short* array, cufftReal*
     }
 }
 
+//TODO: look up best way to reduce an array with CUDA
+//      Currently use first thread in each block to reduce the array corresponding to that block, then return size N array
+//      Once best found, make this function return single integer
+__global__ void calculate_energy(cufftComplex* sample, cufftComplex* combs, int* tempEnergies, int * energies, int sample_size, int N) {
+    int combIdx = blockIdx.x * blockDim.x;
+    int sampleIdx = threadIdx.x;
+
+    if (sampleIdx < sample_size) {
+      int a = sample[sampleIdx].x * combs[combIdx + sampleIdx].x - sample[sampleIdx].y * combs[combIdx + sampleIdx].y;
+      int b = sample[sampleIdx].x * combs[combIdx + sampleIdx].y + sample[sampleIdx].y * combs[combIdx + sampleIdx].x;
+      tempEnergies[combIdx + sampleIdx] = a * a + b * b;
+    }
+
+    if (sampleIdx == 0) {
+      int energy = 0;
+      for (int i=0; i < sample_size; i++) {
+        energy += tempEnergies[combIdx+i];
+      }
+      energies[blockIdx.x] = energy;
+    }
+}
+
+//TODO: write kernel function to do this
+void generateCombs(int BPM_init, int N, int size, int AmpMax, cufftReal* hostDataIn) {
+    for(int i = 0; i < N; i++) {
+      int BPM = BPM_init + i*5;
+      int Ti = 60 * 44100/BPM;
+      int start = size * i; //compute offset for this comfilter
+
+      for(int k = 0; k < size; k+=2) {
+        if (k % Ti == 0) {
+          hostDataIn[start+k] = AmpMax;
+          hostDataIn[start+k+1] = AmpMax;
+        }
+        else {
+          hostDataIn[k] = 0;
+          hostDataIn[k+1] = 0;
+        }
+      }
+    }
+}
+
+void combFilterFFT(int BPM_init, int BPM_final, int N, int size, cufftComplex* deviceDataOut) {
+
+    // Assign Variables
+    cufftHandle plan;
+    cufftReal* deviceDataIn, *hostDataIn;
+
+
+    int AmpMax = 65535;
+     
+    // Malloc Variables 
+    gpuErrchk( cudaMalloc(&deviceDataIn, sizeof(cufftReal) * size * N) );
+//    cudaMalloc(&deviceDataOut, sizeof(cufftComplex) * (size/2 + 1) * N);
+
+    hostDataIn = (cufftReal*)malloc(sizeof(cufftReal) * size * N);
+    
+    //Generate all Combs
+    generateCombs(BPM_init, N, size, AmpMax, hostDataIn);
+
+    int n[1] = {size};
+
+    gpuErrchk( cudaMemcpy(deviceDataIn, hostDataIn, size * N * sizeof(cufftReal), cudaMemcpyHostToDevice) );
+
+    // Now run the fft
+    if (cufftPlanMany(&plan, 1, n, NULL, 1, size, NULL, 1, size, CUFFT_R2C, N) != CUFFT_SUCCESS) {
+        printf("CUFFT Error - plan creation failed\n");
+        exit(-1);
+    }
+    if (cufftExecR2C(plan, deviceDataIn, deviceDataOut) != CUFFT_SUCCESS) {
+        printf("CUFFT Error - execution of FFT failed\n");
+        exit(-1);
+    }
+
+    gpuErrchk( cudaDeviceSynchronize() );
+
+    // Cleanup
+    cufftDestroy(plan);
+    cudaFree(deviceDataIn);
+
+    return;
+}
+
+int combFilterAnalysis(cufftComplex* sample, cufftComplex* combs, int out_size, int N) {
+    //Launch a kernel to calculate the instant energy at position $k$ in the filtered sample, for all k, for all N filters
+
+    //Run Kernel to determine energies
+    int *tempEnergies, *deviceEnergies, *hostEnergies;
+    gpuErrchk( cudaMalloc(&tempEnergies, sizeof(int) * out_size * N) );
+    gpuErrchk( cudaMalloc(&deviceEnergies, sizeof(int) * N) );
+
+    hostEnergies = (int*)malloc(N * sizeof(int));
+
+    int blocks = N; //want a block for each comb
+    int tpb = out_size;
+    //want threads to be power of two
+    tpb--;
+    tpb |= tpb >> 1;
+    tpb |= tpb >> 2;
+    tpb |= tpb >> 4;
+    tpb |= tpb >> 8;
+    tpb |= tpb >> 16;
+    tpb++;
+
+    calculate_energy<<<blocks, tpb>>>(sample, combs, tempEnergies, deviceEnergies, out_size, N);
+    gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
+
+    gpuErrchk( cudaFree(tempEnergies) );
+
+    //Loop through final array to find the best one
+    gpuErrchk( cudaMemcpy(hostEnergies, deviceEnergies, sizeof(int) * N, cudaMemcpyDeviceToHost) );
+
+    //Calculate max of 
+    int max = -1;
+    int index = -1;
+    for (int i = 0; i < 30; i++) {
+        if (hostEnergies[i] > max) {
+            max = hostEnergies[i];
+            index = i;
+        }
+    }
+    
+    return 60 + index * 5;
+}
+
 int BeatCalculatorParallel::cuda_detect_beat(char* s) {
     int max_freq = 4096;
     int sample_size = 2.2 * 2 * max_freq;
@@ -69,7 +195,23 @@ int BeatCalculatorParallel::cuda_detect_beat(char* s) {
         return 0;
     }
 
+    //Create Combs + FFT them
+    cufftComplex* combFFTOut;
+    int BPM_init = 60;
+    int BPM_final = 210;
+    int N = (210-60)/5;
+    gpuErrchk( cudaMalloc(&combFFTOut, sizeof(cufftComplex) * out_size * N) );
+    
+    combFilterFFT(BPM_init, BPM_final, N, sample_size, combFFTOut);
+
+    gpuErrchk(cudaFree(combFFTOut));
+    gpuErrchk(cudaFree(deviceFFTOut));
+
+    //perform analysis
+    int BPM = combFilterAnalysis(deviceFFTOut, combFFTOut, out_size, N);
+
     gpuErrchk(cudaFree(deviceSample));
     gpuErrchk(cudaFree(deviceDifferentiatedSample));
-    return 0;
+
+    return BPM;
 }
